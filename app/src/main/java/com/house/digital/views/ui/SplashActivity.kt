fd9319085a318f791bca168f696fb7271d5f2fab
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-01-25 19:38:17 -0300

    Implementação da splash-screen

---
+++ app/src/main/java/com/house/digital/views/ui/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.house.digital.views.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.house.digital.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
+    }
+
+    private fun init() {
+        binding.imageViewSplash.alpha = 0f
+        binding.imageViewSplash.animate().setDuration(1500).alpha(1f)
+
+        binding.imageViewSplash.alpha = 0f
+        binding.imageViewSplash.animate().setDuration(1500).alpha(1f).withEndAction {
+            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
