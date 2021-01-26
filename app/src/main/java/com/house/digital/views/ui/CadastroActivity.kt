ba87e1626f7ce6b30173ff1f61506d8d5be9f5cb
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-01-25 22:23:57 -0300

    Criação do layout

---
+++ app/src/main/java/com/house/digital/views/ui/CadastroActivity.kt
@@ -0,0 +1,12 @@
+package com.house.digital.views.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.house.digital.R
+
+class CadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
--- app/src/main/java/com/house/digital/views/ui/LoginActivity.kt
+++ app/src/main/java/com/house/digital/views/ui/LoginActivity.kt
@@ -1,12 +1,21 @@
 package com.house.digital.views.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.house.digital.R
+import com.house.digital.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textViewCreateAccount.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, CadastroActivity::class.java))
+        }
     }
 }
