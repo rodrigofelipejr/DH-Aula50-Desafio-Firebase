575224cfc7bf1b1cef17d66ca553cf152e8a2581
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-01-26 18:58:38 -0300

    implementado tela para cadastro do game

---
+++ app/src/main/java/com/house/digital/views/ui/CadastroGameActivity.kt
@@ -0,0 +1,12 @@
+package com.house.digital.views.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.house.digital.R
+
+class CadastroGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_game)
+    }
+}
